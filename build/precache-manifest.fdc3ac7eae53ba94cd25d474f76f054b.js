2d7ebd0ac8529cc24e3842a9bc4614e796f3379b
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2020-10-18 14:31:36 -0700

    first commit

---
+++ build/precache-manifest.fdc3ac7eae53ba94cd25d474f76f054b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdaec72c4e0e360247bdaa34ceba2ac0",
+    "url": "/ordersketch/index.html"
+  },
+  {
+    "revision": "d4404123f6cf5605b9f2",
+    "url": "/ordersketch/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d112e9b791f45aee125a",
+    "url": "/ordersketch/static/css/main.410c1768.chunk.css"
+  },
+  {
+    "revision": "d4404123f6cf5605b9f2",
+    "url": "/ordersketch/static/js/2.4bd849ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ordersketch/static/js/2.4bd849ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d112e9b791f45aee125a",
+    "url": "/ordersketch/static/js/main.b2b50445.chunk.js"
+  },
+  {
+    "revision": "70a86678944c255683aa",
+    "url": "/ordersketch/static/js/runtime-main.84ceb867.js"
+  }
+]);
